e4764f7c59d9d2bd14724501a1dd1dd93a77db3f
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-03-04 19:11:31 +0100

    update sha

--- cmake/HunterConfig.cmake
+++ cmake/HunterConfig.cmake
@@ -1,4 +1,4 @@
 hunter_config(freetype
   URL "https://github.com/zerotacg/freetype/tarball/fix/compatibility-with-case-sensitive-package-name"
-  SHA1 "65a0a497ffc4dddc1a75bdcb3fd0ee26b0690693"
+  SHA1 "1"
 )
