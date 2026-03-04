c695b6078bca60840acb746fb58678e8a43478fd
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-03-04 17:28:40 +0100

    add hunter config

--- cmake/SetupHunter.cmake
+++ cmake/SetupHunter.cmake
@@ -1,12 +1,19 @@
+file(WRITE ${CMAKE_BUILD_DIR}/HunterConfig.cmake [=[
+hunter_config(zlib VERSION 1.2.8)
+
+hunter_config(freetype
+  URL "https://github.com/zerotacg/freetype/tarball/fix/compatibility-with-case-sensitive-package-name"
+  SHA1 "5d311a0ed6acded10e1a5eab44de8c18ec5790c5"
+)
+]=])
+
+
 set(HUNTER_URL "https://github.com/cpp-pm/hunter/archive/v0.26.6.tar.gz")
 set(HUNTER_SHA1 "e70c29f878f5d5f5cdf1b9ccd628fb872e8624a8")
+set(HUNTER_FILEPATH_CONFIG ${CMAKE_BUILD_DIR}/HunterConfig.cmake)
 
 set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
 
-
-set(HUNTER_freetype_version "2.10.4-p2")
-set(HUNTER_freetype_URL "https://github.com/zerotacg/freetype/tarball/fix/compatibility-with-case-sensitive-package-name")
-set(HUNTER_freetype_SHA1 "5d311a0ed6acded10e1a5eab44de8c18ec5790c5")
 
 set(HUNTER_PACKAGES freetype)
 
