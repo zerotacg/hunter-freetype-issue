47250de76d24e9dc9152b6304e7a9385f66e0926
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-03-04 08:05:18 +0100

    adjust package url

--- cmake/SetupHunter.cmake
+++ cmake/SetupHunter.cmake
@@ -4,7 +4,7 @@ set(HUNTER_SHA1 "e70c29f878f5d5f5cdf1b9ccd628fb872e8624a8")
 set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
 
 
-set(HUNTER_freetype_URL "https://github.com/zerotacg/freetype/tree/fix/compatibility-with-case-sensitive-package-name")
+set(HUNTER_freetype_URL "https://github.com/zerotacg/freetype/tarball/fix/compatibility-with-case-sensitive-package-name")
 set(HUNTER_freetype_SHA1 "5d311a0ed6acded10e1a5eab44de8c18ec5790c5")
 
 set(HUNTER_PACKAGES freetype)
